e631480264ee19f5ac2732da84774a0772c0e918
Vladimir Jankovic <vl4di11ir@gmail.com>
2023-11-03 22:26:30 +0100

    Social integration files changed

--- components/Seo/integrations/Google.tsx
+++ components/Seo/integrations/Google.tsx
@@ -1,18 +1,3 @@
 export default function Google() {
-    return (
-        <>
-            <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=UA-96269842-3"
-            ></script>
-            <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag() {
-                dataLayer.push(arguments);
-            }
-            gtag('js', new Date());
-            gtag('config', 'UA-96269842-3');
-            </script>
-        </>
-    )
+  return <>Google integration should be here</>;
 }
